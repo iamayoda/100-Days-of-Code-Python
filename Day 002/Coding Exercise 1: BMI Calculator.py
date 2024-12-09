2019fa9078e19900eebedf2f2825616abb952af4
iamayoda <yandi.mtati@gmail.com>
2024-12-10 00:33:53 +0200

    Create Coding Exercise 1: BMI Calculator.py

---
+++ Day 002/Coding Exercise 1: BMI Calculator.py
@@ -0,0 +1,8 @@
+height = 1.65 
+weight = 84
+
+# Write your code here.
+# Calculate the BMI using weight and height.
+bmi = 84 / 1.65 ** 2 # BMI is equal to the person's weight divided by the person's height squared.
+
+print(bmi)
