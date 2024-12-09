28e8c14b87a10ed7cafbae17bd7fd4173e42d2fc
iamayoda <yandi.mtati@gmail.com>
2024-12-09 23:13:37 +0200

    Update Task 01: Data Types.py

--- Day 002/Task 01: Data Types.py
+++ Day 002/Task 01: Data Types.py
@@ -1,1 +1,29 @@
+#1. SUBSCRIPTING
 
+#1.1 Position of the first character - start from 0
+print("Hello"[0])
+
+#1.2 Position of the last character - start from 0
+print("Hello"[4])
+
+#1.3 Negative indices - "-1" is the last character
+print("Hello"[-1])
+
+#1.4 Negative indices - "-1" is the last character
+print("Hello"[-5])
+
+
+#2. OTHER DATA TYPES
+
+#2.1 "" = String even for numbers (integers) thus the following will be concatenation
+print("123" + "456")
+
+#2.2. Integer - Whole number without ""
+print(123 + 456)
+
+#2.3 Float - Floating point number (decimal)
+print(3.14159)
+
+#2.4 Boolean
+True
+False
