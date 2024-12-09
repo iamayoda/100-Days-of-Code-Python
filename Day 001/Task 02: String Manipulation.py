dd8faa4be409fd73158b5d3bf13f0727fefa0548
iamayoda <yandi.mtati@gmail.com>
2024-12-09 20:47:57 +0200

    Update Task 02: String Manipulation.py

--- Day 001/Task 02: String Manipulation.py
+++ Day 001/Task 02: String Manipulation.py
@@ -3,13 +3,3 @@ print("Hello world!\nHello world!\nHello world!")
 
 #Concatination - Add space between **Hello** and **Ayanda** when running print statement.
 print("Hello " + "Ayanda")
-
-
-#There are errors on all 5 lines of code. Fix the code so that it runs without errors.
-
-#Solution
-print("Notes from Day 1")
-print("The print statement is used to output strings")
-print("String are strings of characters")
-print("String Concatenation is done with the  + sign")
-print("New line can be created a \ and the letter n")
