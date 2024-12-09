1e1605425fc42345dbf587e77cb7d5cc9f87cacb
iamayoda <yandi.mtati@gmail.com>
2024-12-09 14:16:51 +0200

    Update Task 04: Variables.py

--- Day 001/Task 04: Variables.py
+++ Day 001/Task 04: Variables.py
@@ -1,1 +1,8 @@
+#We have 2 variables glass1 and glass2. glass1 contains milk and glass2 contains juice. Write 3 lines of code to switch the contents of the variables. 
+glass1 = "milk"
+glass2 = "juice"
 
+#Solution
+temp = glass1
+glass1 = glass2
+glass2 = temp
