d48efa368609f9b0603c50ce535fabc9655f47b1
Ayanda <yandi.mtati@gmail.com>
2024-12-14 22:38:03 +0200

    Create Project 02: Adventure Title Creator.py

---
+++ Day 001/Project 02: Adventure Title Creator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Personalized Movie Title Maker.")  
+favorite_food = input("What's your favorite food?\n")  
+emotion = input("What's an emotion you feel today?\n")  
+print("Your movie title could be '" + favorite_food + " and the " + emotion + " Adventure.'") #Adding space as part of the text string.
